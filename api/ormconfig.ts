54285e4587579b6cbadd4bad2f958ef0894ca265
armandowni <armando.nurimansyah@gmail.com>
2022-10-12 20:34:00 +0700

    update ormconfig

--- api/ormconfig.ts
+++ api/ormconfig.ts
@@ -7,12 +7,17 @@ export const config: DataSourceOptions = {
   name: "default",
   type: "postgres",
   port: 5432,
-  host: "localhost",
-  username: "postgres",
-  password: "admin123",
-  database: "testingdb",
+  host: "ec2-3-230-122-20.compute-1.amazonaws.com",
+  username: "ndphgigmsldyrf",
+  password: "2f86ab68c36d3be06b46dd7c10bd1422ffa920303040c3081fb48e823b54f913",
+  database: "d3guldico2husd",
   synchronize: true,
   logging: false,
   entities: entities,
   migrations: ["src/migration/**/*.ts"],
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
