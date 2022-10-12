5cea6fe53b47bf8684b3a343e7e6151d662a3eab
armandowni <armando.nurimansyah@gmail.com>
2022-10-12 21:05:14 +0700

    upate index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0 overflow-x-scroll"
   >
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
       <div id="header" class="flex flex-col justify-center pt-8 sm:pt-0">
