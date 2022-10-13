ec111bdfc1818f455e3a67cfa63aff9cf0c72427
armandowni <armando.nurimansyah@gmail.com>
2022-10-13 18:01:01 +0700

    update lagi bwang

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,6 @@ export default defineNuxtConfig({
   meta: {
     title: "nuxt3-typeorm",
   },
-  head: {
-    htmlAttrs: {
-      lang: "en",
-    },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
-    ],
-    // link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  },
   css: ["~/static/global.css", "@fortawesome/fontawesome-svg-core/styles.css"],
   modules: ["@nuxtjs/tailwindcss"],
   vite: { build: { chunkSizeWarningLimit: 10000 } },
