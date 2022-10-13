bad654d783cc45b10dc516b46cf466281f7fbba0
armandowni <armando.nurimansyah@gmail.com>
2022-10-13 17:36:12 +0700

    update api

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,11 @@
-import * as express from "express";
-import cors from "cors";
-import server from "./server";
+import express from "express";
+// import cors from "cors";
+// import server from "./server";
 import bp from "body-parser";
 import { router } from "./routes";
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json({ limit: "20mb" }));
 
