e62bba4274e51ff4e819ef36476b566c67f3f77a
armandowni <armando.nurimansyah@gmail.com>
2022-10-14 16:22:50 +0700

    update orm

---
+++ server/serverorm.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+import { AppDataSource } from "./data-source";
+
+async function server() {
+  await AppDataSource.initialize()
+    .then((conn) => {
+      console.log(`load orm connection`);
+    })
+    .catch((err) =>
+      console.error("Error during Data Source initialization:", err)
+    );
+
+  return ;
+}
+
+export default server;
