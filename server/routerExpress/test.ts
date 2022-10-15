d31fb398436413210a29f0b15a052e9a5ca60e12
armandowni <armando.nurimansyah@gmail.com>
2022-10-15 17:09:19 +0700

    update typeorm

--- server/routerExpress/test.ts
+++ server/routerExpress/test.ts
@@ -5,7 +5,7 @@ import { Test } from "../src/db/entities/Test";
 const router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  const testRepository = AppDataSource
+  // const testRepository = AppDataSource.getRepository(Test)
   // const result = await testRepository.findAndCount();
 
   res.send([[],0]);
