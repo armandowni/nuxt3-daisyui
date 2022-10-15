c8cd98c0a7017a55d555baf46089c7e79cadac40
armandowni <armando.nurimansyah@gmail.com>
2022-10-15 17:13:03 +0700

    update serverorm

--- server/serverorm.ts
+++ server/serverorm.ts
@@ -2,13 +2,13 @@ import "reflect-metadata";
 import { AppDataSource } from "./data-source";
 
 async function server() {
-  await AppDataSource.initialize()
-    .then((conn) => {
-      console.log(`load orm connection`);
-    })
-    .catch((err) =>
-      console.error("Error during Data Source initialization:", err)
-    );
+  // await AppDataSource.initialize()
+  //   .then((conn) => {
+  //     console.log(`load orm connection`);
+  //   })
+  //   .catch((err) =>
+  //     console.error("Error during Data Source initialization:", err)
+  //   );
 
   return ;
 }
