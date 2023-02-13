89583fb818eefb1f8a7abfb2093d3ec350430ca9
Solusindo <armando.nurimansya@gmail.com>
2023-02-13 17:12:34 +0700

    update ormconfig

--- server/database/ormconfig.ts
+++ server/database/ormconfig.ts
@@ -5,12 +5,17 @@ export const config: DataSourceOptions = {
   name: "default",
   type: "postgres",
   port: 5432,
-  host: "localhost",
-  username: "postgres",
-  password: "admin123",
-  database: "testingdb",
+  host: "ec2-44-199-143-43.compute-1.amazonaws.com",
+  username: "amaewbbfiqhhbs",
+  password: "bfb49fb438cca7815bdfb9c0712cf54351cef93a156807ddac0e5c96490deba7",
+  database: "d2nmdu4he1vuf5",
   synchronize: true,
   logging: false,
   entities: entities,
   migrations: ["./util/migration/*.js"],
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
