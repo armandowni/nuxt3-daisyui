c772533ff36d93e99f51043a152a7c459b89d21e
armandowni <56640858+armandowni@users.noreply.github.com>
2023-03-03 11:03:15 +0700

    Update ormconfig.ts

--- server-middleware/ormconfig.ts
+++ server-middleware/ormconfig.ts
@@ -5,17 +5,12 @@ export const config: DataSourceOptions = {
   name: "default",
   type: "postgres",
   port: 5432,
-  host: "ec2-44-199-143-43.compute-1.amazonaws.com",
-  username: "amaewbbfiqhhbs",
-  password: "bfb49fb438cca7815bdfb9c0712cf54351cef93a156807ddac0e5c96490deba7",
-  database: "d2nmdu4he1vuf5",
+  host: "localhost",
+  username: "postgres",
+  password: "admin123",
+  database: "postgres",
   synchronize: true,
   logging: false,
   entities: entities,
   migrations: ["./util/migration/*.js"],
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
 };
